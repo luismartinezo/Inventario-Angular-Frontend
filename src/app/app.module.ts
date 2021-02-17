f3bd6b0af5a1b711fa3ce7fd2ca8157e973a69d2
luismartinezo <lmartinezocoro@gmail.com>
2021-02-16 20:32:39 -0500

    3 files modified, 6 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./Shared/footer/footer.component";
+import { NavbarComponent } from "./Shared/navbar/navbar.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
+  declarations: [AppComponent, FooterComponent, NavbarComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
