3506b2767b86d507ec087d141f85fd12c69ac87f
luismartinezo <lmartinezocoro@gmail.com>
2021-02-17 07:42:15 -0500

    agregar producto o mercancia

--- src/app/Components/mercancia/nuevo-producto/nuevo-producto.component.ts
+++ src/app/Components/mercancia/nuevo-producto/nuevo-producto.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Mercancia } from "../../../Models/mercancia";
+import { ProductoService } from "../../../Services/producto.service";
 
 @Component({
-  selector: 'app-nuevo-producto',
-  templateUrl: './nuevo-producto.component.html',
-  styleUrls: ['./nuevo-producto.component.css']
+  selector: "app-nuevo-producto",
+  templateUrl: "./nuevo-producto.component.html",
+  styleUrls: ["./nuevo-producto.component.css"]
 })
 export class NuevoProductoComponent implements OnInit {
+  form: any = {
+    id: 0,
+    nombre_producto: "",
+    cantidad: 0,
+    fecha_ingreso: Date,
+    usuario_id: 1
+  };
+  productos: Mercancia;
+  mensajeOK = "";
+  mensajeFail = "";
+  constructor(
+    private productoService: ProductoService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onCreate(form): void {
+    console.log(form);
+    this.productoService.crear(this.form).subscribe(
+      data => {
+        this.mensajeOK = data.message;
+        alert(this.mensajeOK);
+        this.router.navigate(["productos"]);
+      },
+      (err: any) => {
+        this.mensajeFail = err.error.message;
+      }
+    );
   }
-
 }
