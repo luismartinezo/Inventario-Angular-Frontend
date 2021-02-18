abe98e328ee9673ff36dc142bfb6629a6bb6d089
luismartinezo <lmartinezocoro@gmail.com>
2021-02-18 18:23:05 -0500

    1 file modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,13 +16,13 @@ import { NuevoUsuarioComponent } from './Components/usuario/nuevo-usuario/nuevo-
 import { NuevoCargoComponent } from './Components/cargo/nuevo-cargo/nuevo-cargo.component';
 import { UsuarioService } from './Services/usuario.service';
 import { CargoService } from './Services/cargo.service';
-import { EditarCargoComponent } from './Components/cargo/editar-cargo/editar-cargo.component';
-import { DetalleCargoComponent } from './Components/cargo/detalle-cargo/detalle-cargo.component';
 import { EditarProductoComponent } from './Components/mercancia/editar-producto/editar-producto.component';
 import { DetalleProductoComponent } from './Components/mercancia/detalle-producto/detalle-producto.component';
 import { EditarUsuarioComponent } from './Components/usuario/editar-usuario/editar-usuario.component';
 import { DetalleUsuarioComponent } from './Components/usuario/detalle-usuario/detalle-usuario.component';
-import { HomeComponent } from './Components/home/home.component'
+import { HomeComponent } from './Components/home/home.component';
+import { DetalleCargoComponent } from './Components/cargo/detalle-cargo/detalle-cargo.component';
+import { EditarCargoComponent } from './Components/cargo/editar-cargo/editar-cargo.component'
 
 @NgModule({
   imports: [
@@ -32,7 +32,7 @@ import { HomeComponent } from './Components/home/home.component'
     ReactiveFormsModule,
     AppRoutingModule
   ],
-  declarations: [AppComponent, FooterComponent, NavbarComponent, CargoComponent, UsuarioComponent, MercanciaComponent, NuevoProductoComponent, NuevoUsuarioComponent, NuevoCargoComponent, EditarCargoComponent, DetalleCargoComponent, EditarProductoComponent, DetalleProductoComponent, EditarUsuarioComponent, DetalleUsuarioComponent, HomeComponent],
+  declarations: [AppComponent, FooterComponent, NavbarComponent, CargoComponent, UsuarioComponent, MercanciaComponent, NuevoProductoComponent, NuevoUsuarioComponent, NuevoCargoComponent,  EditarProductoComponent, DetalleProductoComponent, EditarUsuarioComponent, DetalleUsuarioComponent, HomeComponent, DetalleCargoComponent, EditarCargoComponent],
   bootstrap: [AppComponent],
   providers: [ProductoService, UsuarioService, CargoService]
 })
